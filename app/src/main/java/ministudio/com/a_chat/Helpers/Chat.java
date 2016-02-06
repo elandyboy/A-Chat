8cbceb13bfd5ed2d2e91a0cfba8e087aefeb2a5f
elandyboy <toxicjosef14@gmail.com>
2016-02-06 14:56:24 -0700

    Login
    Login layout ready.

--- app/src/main/java/ministudio/com/a_chat/Helpers/Chat.java
+++ app/src/main/java/ministudio/com/a_chat/Helpers/Chat.java
@@ -1,7 +1,18 @@
 package ministudio.com.a_chat.Helpers;
 
+import com.parse.ParseObject;
+
 /**
  * Created by elandy on 2/5/16.
  */
-public class Chat {
+public class Chat extends ParseObject {
+    public void setMessage(String message){
+        put("message",message);
+
+    }
+    public String getMessage(){
+        return getString("message");
+
+    }
+
 }
